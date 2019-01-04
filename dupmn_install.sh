e157223c1b6019b8d63414827f1495d8f0a4a283
neo3587 <neo3587@users.noreply.github.com>
2019-01-04 05:07:27 +0100

    added ascii art logo + source link

--- dupmn_install.sh
+++ dupmn_install.sh
@@ -6,8 +6,21 @@ NC='\e[0m'
 rm -rf ./dupmn.sh
 wget -q https://raw.githubusercontent.com/neo3587/dupmn/master/dupmn.sh
 
+echo -e "\n===================================================\
+         \n   ██████╗ ██╗   ██╗██████╗ ███╗   ███╗███╗   ██╗  \
+         \n   ██╔══██╗██║   ██║██╔══██╗████╗ ████║████╗  ██║  \
+         \n   ██║  ██║██║   ██║██████╔╝██╔████╔██║██╔██╗ ██║  \
+         \n   ██║  ██║██║   ██║██╔═══╝ ██║╚██╔╝██║██║╚██╗██║  \
+         \n   ██████╔╝╚██████╔╝██║     ██║ ╚═╝ ██║██║ ╚████║  \
+         \n   ╚═════╝  ╚═════╝ ╚═╝     ╚═╝     ╚═╝╚═╝  ╚═══╝  \
+         \n                                ╗ made by neo3587 ╔\
+         \n           Source: https://github.com/neo3587/dupmn\
+         \n  BTC Donations: 3HE1kwgHEWvxBa38NHuQbQQrhNZ9wxjhe7\
+         \n===================================================\
+         \n                                                   "
+
 if [[ -f /usr/bin/dupmn && ! $(diff -q ./dupmn.sh /usr/bin/dupmn) ]]; then
-	echo -e "${GREEN}dupmn${NC} is already updated to the last version"
+	echo -e "${GREEN}dupmn${NC} is already updated to the last version\n"
 	rm -rf ./dupmn.sh
 	exit
 fi
@@ -23,7 +36,7 @@ mv ./dupmn.sh /usr/bin/dupmn
 chmod +x /usr/bin/dupmn
 
 if [[ $update = "1" ]]; then
-	echo -e "${GREEN}dupmn${NC} updated to the last version, pretty fast, right?"
+	echo -e "${GREEN}dupmn${NC} updated to the last version, pretty fast, right?\n"
 else 
-	echo -e "${GREEN}dupmn${NC} installed, pretty fast, right?"
+	echo -e "${GREEN}dupmn${NC} installed, pretty fast, right?\n"
 fi
